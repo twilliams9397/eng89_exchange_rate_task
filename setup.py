d7b1dde92d7619e2e5faa4b676c942487bb431fb
Tom <Tom1@MacBook-Pro-2.local>
2021-06-30 14:24:37 +0100

    all files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# add some information about package
+setup(name="Currency Exchange App") # this is required, lines below are optional
+version = "1.0"
+description = "Python app"
+author = "Tom at Sparta Global"
+author_email = "twilliams@spartaglobal.com"
+url = "http://spartaglobal.com"
